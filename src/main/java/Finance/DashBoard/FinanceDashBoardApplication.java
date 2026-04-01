b3b8ef21e6e9be92c6dbbabbfde139d35dc76bec
MohdSadiuqe <140594137+MohdSadiuqe@users.noreply.github.com>
2026-04-01 21:12:40 +0530

    First Commit

---
+++ src/main/java/Finance/DashBoard/FinanceDashBoardApplication.java
@@ -0,0 +1,13 @@
+package Finance.DashBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceDashBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceDashBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Finance/DashBoard/FinanceDashBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package Finance.DashBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceDashBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
